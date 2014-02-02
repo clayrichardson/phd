25ca17ff65b4b557982f5b6afa937380b2aa038f
clay richardson <c.richardson917@gmail.com>
2014-02-02 04:03:58 +0000

    added ./setup/phabricator.sh

---
+++ setup/phabricator.sh
@@ -0,0 +1,1 @@
+mkdir -p /var/nginx/cache/phab
--- setup/setup.sh
+++ setup/setup.sh
@@ -3,3 +3,4 @@ set -xe
 /bin/bash /setup/apt-key.sh
 /bin/bash /setup/supervisor.sh
 /bin/bash /setup/latest_nginx.sh
+/bin/bash /setup/phabricator.sh
