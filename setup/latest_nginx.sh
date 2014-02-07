7d4a99cec07757229cd6f808842227e880fed86f
clay richardson <c.richardson917@gmail.com>
2014-02-07 04:56:19 +0000

    removed uncessary setup scripts

--- setup/latest_nginx.sh
+++
@@ -1,6 +0,0 @@
-set -xe
-apt-get install -qy python-software-properties
-add-apt-repository -y ppa:nginx/stable
-apt-get update
-apt-get install -qy nginx
-nginx -v
--- setup/supervisor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xe
-apt-get install -qy supervisor
-mkdir -p /var/run/nginx
-mkdir -p /var/log/supervisor
