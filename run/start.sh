c9ded4462fc2c8ebf2d11a38a12f1ed4fc95c870
clay richardson <c.richardson917@gmail.com>
2014-02-08 14:03:41 +0000

    added nice value to daemons

--- run/start.sh
+++ run/start.sh
@@ -2,5 +2,5 @@
 set -xe
 /var/www/phabricator/bin/storage upgrade --force --trace
 /usr/bin/supervisord
-sudo -u www-data /var/www/phabricator/bin/phd start
+nice -n 20 sudo -u www-data /var/www/phabricator/bin/phd start
 tail -F /var/log/supervisor/*.log /var/log/nginx/*.log
