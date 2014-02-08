4cb17f821b44105bf2818c7caf73d15fb6883033
clay richardson <c.richardson917@gmail.com>
2014-02-08 06:09:05 +0000

    added --trace to storage upgrade

--- run/start.sh
+++ run/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
-/var/www/phabricator/bin/storage upgrade --force
+/var/www/phabricator/bin/storage upgrade --force --trace
 /usr/bin/supervisord
 tail -F /var/log/supervisor/*.log /var/log/nginx/*.log
