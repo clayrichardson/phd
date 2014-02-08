f10fae28876ee3901dbff3f4afa073cd5166ec16
clay richardson <c.richardson917@gmail.com>
2014-02-08 11:57:41 +0000

    sudo -u www-data phd start

--- run/start.sh
+++ run/start.sh
@@ -2,5 +2,5 @@
 set -xe
 /var/www/phabricator/bin/storage upgrade --force --trace
 /usr/bin/supervisord
-/var/www/phabricator/bin/phd start
+sudo -u www-data /var/www/phabricator/bin/phd start
 tail -F /var/log/supervisor/*.log /var/log/nginx/*.log
