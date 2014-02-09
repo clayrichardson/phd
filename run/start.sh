9e9ac42b4be8a49611eb8c9d7d73305b7968640d
clay richardson <c.richardson917@gmail.com>
2014-02-09 04:21:04 +0000

    added attribution to start.sh

--- run/start.sh
+++ run/start.sh
@@ -3,6 +3,7 @@ set -xe
 /var/www/phabricator/bin/storage upgrade --force --trace
 /usr/bin/supervisord
 nice -n 20 sudo -u www-data /var/www/phabricator/bin/phd start
+# adapted from: http://stackoverflow.com/a/20932423/1050649
 while ( true )
   do
   echo "Detach with Ctrl-p Ctrl-q. Dropping to shell"
