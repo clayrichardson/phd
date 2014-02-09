0da637734093f81aea2080210a009aa37bbc6536
clay richardson <c.richardson917@gmail.com>
2014-02-09 04:19:51 +0000

    added maintenance shell to start.sh

--- run/start.sh
+++ run/start.sh
@@ -3,4 +3,9 @@ set -xe
 /var/www/phabricator/bin/storage upgrade --force --trace
 /usr/bin/supervisord
 nice -n 20 sudo -u www-data /var/www/phabricator/bin/phd start
-tail -F /var/log/supervisor/*.log /var/log/nginx/*.log
+while ( true )
+  do
+  echo "Detach with Ctrl-p Ctrl-q. Dropping to shell"
+  sleep 1
+  /bin/bash
+done
