a6a3536a87ba5772aebb8f42d923bf5031a71ada
clay richardson <c.richardson917@gmail.com>
2014-02-10 04:53:37 +0000

    setup.sh now checks for swap and groups

--- setup.sh
+++ setup.sh
@@ -4,44 +4,47 @@ set -ex
  
 # Swap file commands from:
 # https://www.digitalocean.com/community/articles/how-to-add-swap-on-ubuntu-12-04
- 
-# Create swapfile
-echo "Creating swap file..."
-dd if=/dev/zero of=/swapfile bs=1024 count=4096k
- 
-# Create linux swap area
-mkswap /swapfile
- 
-# Activate the swap file
-swapon /swapfile
- 
-# Display summary
-swapon -s
- 
-# Add entry to fstab
-# Notice: running this script multiple times
-# will append duplicate entries in fstab
-echo "/swapfile none swap sw 0 0" >> /etc/fstab
- 
-# Set swapiness to 0, so swap is only used when
-# physical memory is not available
-echo 0 | sudo tee /proc/sys/vm/swappiness
-echo vm.swappiness = 0 | sudo tee -a /etc/sysctl.conf
- 
-# Change ownership, so only root can view
-chown root:root /swapfile
-chmod 0600 /swapfile
- 
+
+if [ $(free | grep -i swap | awk '{print $2}') -eq 0 ]
+then
+  # Create swapfile
+  echo "Creating swap file..."
+  dd if=/dev/zero of=/swapfile bs=1024 count=4096k
+   
+  # Create linux swap area
+  mkswap /swapfile
+   
+  # Activate the swap file
+  swapon /swapfile
+   
+  # Display summary
+  swapon -s
+   
+  # Add entry to fstab
+  # Notice: running this script multiple times
+  # will append duplicate entries in fstab
+  echo "/swapfile none swap sw 0 0" >> /etc/fstab
+   
+  # Set swapiness to 0, so swap is only used when
+  # physical memory is not available
+  echo 0 | sudo tee /proc/sys/vm/swappiness
+  echo vm.swappiness = 0 | sudo tee -a /etc/sysctl.conf
+   
+  # Change ownership, so only root can view
+  chown root:root /swapfile
+  chmod 0600 /swapfile
+fi
+
 # Some useful tools I like to use
 # You may omit this line if you wish
 apt-get install -y htop git bmon iotop traceroute
 
-\curl -sSL https://get.docker.io
+curl -sSL https://get.docker.io | bash
  
 # Add the docker group if it doesn't already exist.
 
 getent group docker
-if [ $? -ne 0 ] ;
+if [ -z $(getent group docker) ]
 then
   echo "group docker does not exist, adding..."
   sudo groupadd docker
@@ -56,5 +59,5 @@ sudo gpasswd -a ubuntu docker
 # Restart the docker daemon.
 sudo service docker restart
  
-\curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3
+curl -sSL https://get.rvm.io | bash -s stable --ruby=1.9.3
 
